633707fde1d2a7176fdbff013c44083974d0a444
Nouman Mehmood <nouman.mehmood-cic-uk@ibm.com>
2021-02-17 13:57:32 +0000

    Add basic tests

--- FlickrFinderTests/FlickrFinderTests.swift
+++ FlickrFinderTests/FlickrFinderTests.swift
@@ -12,6 +12,15 @@ class FlickrFinderTests: XCTestCase {
 
     let main = ViewController()
     
+    // Check JSON returns not nill
+    func testNilJSONData(){
+        let result: () = main.getResponse(tags: "car", pageParam: 1)
+        XCTAssertNotNil(result)
+    }
     
-
+    // Check JSON returns nill
+    func testJSONData(){
+        let result: () = main.getResponse(tags: "aadwdwfwefwfwfwfwff", pageParam: 1)
+        XCTAssertNil(result)
+    }
 }
